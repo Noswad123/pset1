b7442808d7fda42e9385d83b6631ed22e66a9585
Jamal Dawson <>
2018-04-18 22:36:48 +0000

    post check50

--- cash.c
+++ cash.c
@@ -1,48 +1,57 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
 
-int main (void)
+int main(void)
 {
-  int changeOwed=0;
-  int dimes=0;
-  int quarters=0;
-  int nickels=0;
-  int pennies=0;
-  int coinCounter=0;
-  float temp=0;
+    int changeOwed = 0;
+    int dimes = 0;
+    int quarters = 0;
+    int nickels = 0;
+    int pennies = 0;
+    int coinCounter = 0;
+    float temp = 0;
 
-  do{
-    temp=get_float("Change owed: ");
-  }while(temp==0);
+    do
+    {
+        temp = get_float("Change owed: ");
+    }
+    while (temp == 0);
 
-  changeOwed=temp/.01;
-    if(changeOwed>=25)
+    temp =  roundf(temp * 100);
+    changeOwed = temp;
+
+    if (changeOwed >= 25)
     {
-        quarters=changeOwed/25;
-        coinCounter+=quarters;
-        changeOwed=changeOwed%25;
+        quarters = changeOwed / 25;
+        coinCounter += quarters;
+        changeOwed = changeOwed % 25;
     }
-      if(changeOwed>=10)
+
+    if (changeOwed >= 10)
     {
-        dimes=changeOwed/10;
-        coinCounter+=dimes;
-        changeOwed=changeOwed%10;
+        dimes = changeOwed / 10;
+        coinCounter += dimes;
+        changeOwed = changeOwed % 10;
     }
-      if(changeOwed>=5)
+    if (changeOwed >= 5)
     {
-        dimes=changeOwed/5;
-        coinCounter+=dimes;
-        changeOwed=changeOwed%5;
+        nickels = changeOwed / 5;
+        coinCounter += nickels;
+        changeOwed = changeOwed % 5;
     }
-      if(changeOwed>=1)
+
+    if (changeOwed >= 1)
     {
-        pennies=changeOwed/1;
-        coinCounter+=pennies;
-        changeOwed=changeOwed%1;
+        pennies = changeOwed / 1;
+        coinCounter += pennies;
+        changeOwed = changeOwed % 1;
     }
-    printf("Coins used %i \n", coinCounter);
-    printf("quarters: %i, dimes: %i, nickels: %i, pennies: %i \n",quarters, dimes, nickels, pennies);
+    printf("%i \n", coinCounter);
+    //extra info
+    //printf("Coins used %i \n", coinCounter);
+    //printf("quarters: %i, dimes: %i, nickels: %i, pennies: %i \n",quarters, dimes, nickels, pennies);
 }
 
 
--- mario.c
+++ mario.c
@@ -2,37 +2,43 @@
 #include <stdio.h>
 
 
-int main (void)
+int main(void)
 {
-    int height=0;
-    bool isValid=false;
-    do{
+    int height = 0;
+    bool isValid = false;
+    do
+    {
         height = get_int("How high would you like the pyramid? 1-23: ");
-
-        if(height>0 || height<24){
-            isValid=true;
+        if (height >=0 && height < 24)
+        {
+            isValid = true;
         }
-    }while(!isValid);
-
-
-    for(int i=height;i>0;i--){
-        for(int k=0;k<i;k++){
-                printf(" ");
-            }
-        for(int j=0;j<(height-i);j++){
-
-            printf("#");
-
-        }
-        printf("  ");
-        for(int j=0;j<(height-i);j++){
-
-            printf("#");
-
-        }
-        printf("\n");
-
     }
+    while (!isValid);
 
 
+    for (int i = height - 1; i >= 0; i--)
+    {
+        for (int k = 0; k < i; k++)
+        {
+
+            printf(" ");
+
+        }
+        for (int j = 0; j < (height - i); j++)
+        {
+            printf("#");
+        }
+        //for mario more uncomment this section
+        // printf("  ");
+        // for (int j = 0; j < (height - i); j++)
+        // {
+        //     printf("#");
+        // }
+
+        if (i != height)
+        {
+            printf("\n");
+        }
+    }
 }
